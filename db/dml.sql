a11b01a4f6b2b203eb1bf49e1570d2b30595936a
dieguitozz <diegolenda2006@gmail.com>
2025-03-03 22:24:51 -0300

    extra database

--- db/dml.sql
+++ db/dml.sql
@@ -1,4 +1,9 @@
 INSERT INTO tb_usuarios (nome, telefone, email, senha)
 VALUES ('Diego', '11999999999', 'diego@email.com', 'senha123');
 
+INSERT INTO tb_barbeiros (nome, telefone, endereco, numero)
+VALUES ('Carlos Silva', '11988887777', 'Rua das Palmeiras', 316);
+
 select * from usuarios
+
+select * from tb_barbeiros
