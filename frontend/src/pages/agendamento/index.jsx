3c99c3682ea86f443be10d18152ef6281fda853e
dieguitozz <diegolenda2006@gmail.com>
2025-03-04 21:05:52 -0300

    side menu agendamento

--- frontend/src/pages/agendamento/index.jsx
+++ frontend/src/pages/agendamento/index.jsx
@@ -1,39 +1,45 @@
-import Cabecalho from '../../components/cabecalho';
+import MenuLateral from "../../components/menuLateral"
 import './index.scss';
 import Calendario from '../../components/calendario';
 import CardHorario from '../../components/cardHorario';
+import { Link } from "react-router-dom";
 import Rodape from '../../components/rodape';
 
 export default function Agendamento() {
     return (
 
         <div className='secao-agendamento'>
-            <Cabecalho />
-            <Calendario />
-
-            <div className="title">
-                <h1>HORÀRIOS</h1>
-                <div className="borda"></div>
+            <div>
+                <MenuLateral />
             </div>
+            <div>
+                <div className="content">
+                    <Calendario />
 
-            <div className="cards">
-                <CardHorario horario={"08:00"} />
-                <CardHorario horario={"08:30"} />
-                <CardHorario horario={"09:00"} />
-                <CardHorario horario={"10:00"} />
-                <CardHorario horario={"10:30"} />
-                <CardHorario horario={"11:00"} />
-                <CardHorario horario={"11:30"} />
-                <CardHorario horario={"12:00"} />
-                <CardHorario horario={"12:30"} />
+                    <div className="title">
+                        <h1>Horários</h1>
+                        <div className="borda"></div>
+                    </div>
 
-                <div className="botao">
-                    <button>Confirmar agendamento</button>
+                    <div className="cards">
+                        <CardHorario horario={"08:00"} />
+                        <CardHorario horario={"08:30"} />
+                        <CardHorario horario={"09:00"} />
+                        <CardHorario horario={"10:00"} />
+                        <CardHorario horario={"10:30"} />
+                        <CardHorario horario={"11:00"} />
+                        <CardHorario horario={"11:30"} />
+                        <CardHorario horario={"12:00"} />
+                        <CardHorario horario={"12:30"} />
+
+                        <div className="botao">
+                            <button>Confirmar agendamento</button>
+                        </div>
+                    </div>
                 </div>
             </div>
-
-            <Rodape />
         </div>
+
     );
 }
 
